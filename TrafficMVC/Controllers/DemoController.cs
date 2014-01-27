72109e8621eb445e429a39e9a797608a4e1f79ce
Jevgeni <zeka.rum@gmail.com>
2014-01-27 20:13:42 +0200

    Added angularUI

---
+++ TrafficMVC/Controllers/DemoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrafficMVC.Controllers
+{
+    public class DemoController : Controller
+    {
+        public ActionResult AutoComplete()
+        {
+            return View();
+        }
+	}
+}
