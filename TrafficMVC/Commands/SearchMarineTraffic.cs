d35c865c2ca60b83b9787571bf9dab8cfde101b9
Jevgeni <zeka.rum@gmail.com>
2014-01-30 00:18:38 +0200

    try-catch block for parser - all info gathered before occurance of the error will be displayed in create form.

--- TrafficMVC/Commands/SearchMarineTraffic.cs
+++ TrafficMVC/Commands/SearchMarineTraffic.cs
@@ -21,47 +21,54 @@ namespace TrafficMVC.Commands
 
         public bool Execute()
         {
-            if (Imo.IsNullOrWhiteSpace())
-                return false;
-
-            // Get data from page
-            WebClient client = new WebClient();
-            String htmlCode = client.DownloadString(string.Format("http://maritime-connector.com/ship/{0}/", Imo)); // imo = 9100932
-
-            // Search all table rows
-            HtmlDocument document = new HtmlDocument();
-            document.LoadHtml(htmlCode);
-            HtmlNodeCollection allTableRows = document.DocumentNode.SelectNodes("//tr");
-
-            // Exit if no rows were found
-            if (allTableRows == null)
-                return false;
-
-            foreach (HtmlNode tableRow in allTableRows)
+            try
             {
-                // skip this table row if it does not contain anything
-                if (!tableRow.HasChildNodes) continue;
+                if (Imo.IsNullOrWhiteSpace())
+                    return false;
 
-                var key = "";
-                var value = "";
+                // Get data from page
+                WebClient client = new WebClient();
+                String htmlCode = client.DownloadString(string.Format("http://maritime-connector.com/ship/{0}/", Imo)); // imo = 9100932
 
-                // Get header name
-                foreach (var tableHeaderCell in tableRow.SelectNodes("th"))
+                // Search all table rows
+                HtmlDocument document = new HtmlDocument();
+                document.LoadHtml(htmlCode);
+                HtmlNodeCollection allTableRows = document.DocumentNode.SelectNodes("//tr");
+
+                // Exit if no rows were found
+                if (allTableRows == null)
+                    return false;
+
+                foreach (HtmlNode tableRow in allTableRows)
                 {
-                    key = tableHeaderCell.InnerHtml.Trim();
-                    break;
-                }
+                    // skip this table row if it does not contain anything
+                    if (!tableRow.HasChildNodes) continue;
 
-                // Get value name
-                foreach (var tableCell in tableRow.SelectNodes("td"))
-                {
-                    value = tableCell.InnerHtml.Trim();
-                    break;
-                }
+                    var key = "";
+                    var value = "";
 
-                // check only if key exists, and store key-value pair
-                if (!key.IsNullOrWhiteSpace())
-                    marineTrafficData.Add(key, value);
+                    // Get header name
+                    foreach (var tableHeaderCell in tableRow.SelectNodes("th"))
+                    {
+                        key = tableHeaderCell.InnerHtml.Trim();
+                        break;
+                    }
+
+                    // Get value name
+                    foreach (var tableCell in tableRow.SelectNodes("td"))
+                    {
+                        value = tableCell.InnerHtml.Trim();
+                        break;
+                    }
+
+                    // check only if key exists, and store key-value pair
+                    if (!key.IsNullOrWhiteSpace())
+                        marineTrafficData.Add(key, value);
+                }
+            }
+            catch
+            {
+                
             }
 
             marineTrafficData["Success"] = "true";
